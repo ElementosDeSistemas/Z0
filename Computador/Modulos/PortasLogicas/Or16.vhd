cfc4179dfc0f31f061a1e0a7e5106cc3b1204b94
Marcelo Prado <marceloterreiroprado@gmail.com>
2017-03-03 08:22:14 -0300

    or16

--- Computador/Modulos/PortasLogicas/Or16.vhd
+++ Computador/Modulos/PortasLogicas/Or16.vhd
@@ -2,8 +2,29 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Or16 is
-	port ( 
+	port (
 			a:   in  STD_LOGIC_VECTOR(15 downto 0);
 			b:   in  STD_LOGIC_VECTOR(15 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+
+
+architecture arch_or16 of Or16 is
+	begin
+	q(0) <= or a(0);
+	q(1) <= or a(1);
+	q(2) <= or a(2);
+	q(3) <= or a(3);
+	q(4) <= or a(4);
+	q(5) <= or a(5);
+	q(6) <= or a(6);
+	q(7) <= or a(7);
+	q(8) <= or a(8);
+	q(9) <= or a(9);
+	q(10) <= or a(10);
+	q(11) <= or a(11);
+	q(12) <= or a(12);
+	q(13) <= or a(13);
+	q(14) <= or a(14);
+	q(15) <= or a(15);
+end arch_or16;
