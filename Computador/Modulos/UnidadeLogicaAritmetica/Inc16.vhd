9b2857eabc139ddd82820636d15ee6d42f2d662b
SabrinaSimao <sabrina_simao@hotmail.com>
2017-03-09 13:54:37 -0300

    Inc16 teste 1

--- Computador/Modulos/UnidadeLogicaAritmetica/Inc16.vhd
+++ Computador/Modulos/UnidadeLogicaAritmetica/Inc16.vhd
@@ -9,4 +9,20 @@ entity Inc16 is
 		a   :  in STD_LOGIC_VECTOR(15 downto 0);
 		q   : out STD_LOGIC_VECTOR(15 downto 0) 
 	);
-end entity; 
+end entity;
+
+architecture a_inc16 of Inc16 is
+
+component Add16 is
+	port(
+		a   :  in STD_LOGIC_VECTOR(15 downto 0);
+		b   :  in STD_LOGIC_VECTOR(15 downto 0);
+		q   : out STD_LOGIC_VECTOR(15 downto 0) 
+	); 
+end component;
+
+Begin
+
+q <= Add16(a,"0000000000000001");
+
+end a_inc16;
