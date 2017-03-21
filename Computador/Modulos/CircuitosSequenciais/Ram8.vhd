a88f9feed799841ea07c53623a59c6e0a16eef0a
SabrinaSimao <sabrina_simao@hotmail.com>
2017-03-21 15:49:35 -0300

    Comecei ram8 mas nao terminei

--- Computador/Modulos/CircuitosSequenciais/Ram8.vhd
+++ Computador/Modulos/CircuitosSequenciais/Ram8.vhd
@@ -14,3 +14,20 @@ entity Ram8 is
 		output:  out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
+architecture ram8 of Ram8 is
+
+Component Register16 is
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(15 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(15 downto 0)
+	);
+end component;
+signal index : STD_LOGIC_VECTOR(7 downto 0);
+begin
+
+p1: Register16 port map (clock,input(15 downto 0),index(0),output(15 downto 0));
+p2: Register16 port map (clock,input(15 downto 0),load,output(15 downto 0));
+
+end architecture;
