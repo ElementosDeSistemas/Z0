da9b478b2535d3049dc3ee60dd2d9e79365739a9
Leostayner <leostayner@gmail.com>
2017-03-22 20:50:43 -0300

    v1

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterSIPO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterSIPO16.vhd
@@ -15,3 +15,27 @@ entity ShiftRegisterSIPO16 is
 		output: out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
+
+-- Componentes
+
+component FlipFlopD is
+	port(
+		clock:  in std_logic;
+		d:      in std_logic;
+		clear:  in std_logic;
+		preset: in std_logic;
+		q:     out std_logic
+	);
+end component;
+
+architecture arch_SIPO of ShiftRegisterSIPO16 is
+
+signal saida_0,saida_1,saida_2: STD_LOGIC_VECTOR(15 downto 0);
+
+begin
+	w : port map FlipFlopD(clock,input,'1','1',saida_0);
+	w1 : port map FlipFlopD(clock,saida_0,'1','1',saida_1);
+	w2 : port map FlipFlopD(clock,saida_1,'1','1',saida_2);
+	w3 : port map FlipFlopD(clock,saida_2,'1','1',q);
+
+end architecture;
