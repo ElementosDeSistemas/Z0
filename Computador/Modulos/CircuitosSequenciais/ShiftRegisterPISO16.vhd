2f564b18e21c4816ab7d69d951f16d907efc0e14
SabrinaSimao <sabrina_simao@hotmail.com>
2017-03-22 19:24:47 -0300

    tentei começar o piso mas esta triste haha

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
@@ -18,3 +18,39 @@ entity ShiftRegisterPISO16 is
 		output: out STD_LOGIC
 	);
 end entity;
+architecture piso16 of ShiftRegisterPISO16 is
+
+component FlipFlopD is
+	port(
+		clock:  in std_logic;
+		d:      in std_logic;
+		clear:  in std_logic;
+		preset: in std_logic;
+		q:     out std_logic
+	);
+end component;
+--signal tmp: STD_LOGIC_VECTOR(15 downto 0);
+-- paulo, nao sei direito escrever, apesar deu ter entendido como funciona o shifter,
+-- a gramatica esta me pegando aqui, entao acho que esta beeem errado, haha
+begin
+
+p01: FlipFlopD port map (clock,input(15 downto 0),load,output(15 downto 0));
+p02: FlipFlopD port map (clock,input(p01(output)),load,output(15 downto 0));
+p03: FlipFlopD port map (clock,input(p02(output)),load,output(15 downto 0));
+p04: FlipFlopD port map (clock,input(p03(output)),load,output(15 downto 0));
+p05: FlipFlopD port map (clock,input(p04(output)),load,output(15 downto 0));
+p06: FlipFlopD port map (clock,input(p05(output)),load,output(15 downto 0));
+p07: FlipFlopD port map (clock,input(p06(output)),load,output(15 downto 0));
+p08: FlipFlopD port map (clock,input(p07(output)),load,output(15 downto 0));
+p09: FlipFlopD port map (clock,input(p08(output)),load,output(15 downto 0));
+p10: FlipFlopD port map (clock,input(p09(output)),load,output(15 downto 0));
+p11: FlipFlopD port map (clock,input(p10(output)),load,output(15 downto 0));
+p12: FlipFlopD port map (clock,input(p11(output)),load,output(15 downto 0));
+p13: FlipFlopD port map (clock,input(p12(output)),load,output(15 downto 0));
+p14: FlipFlopD port map (clock,input(p13(output)),load,output(15 downto 0));
+p15: FlipFlopD port map (clock,input(p14(output)),load,output(15 downto 0));
+p16: FlipFlopD port map (clock,input(p15(output)),load,output(15 downto 0));
+
+output <= p16(output);
+
+end architecture;
