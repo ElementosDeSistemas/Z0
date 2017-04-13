dcfd89986eea605eca93a9489dd7bb399b5fa131
pedrodelapena <pedrodelapena@uol.com.br>
2017-04-13 18:31:25 -0300

    aaa

--- Computador/Modulos/CPU/ControlUnit.vhd
+++ Computador/Modulos/CPU/ControlUnit.vhd
@@ -1,3 +1,4 @@
+
 -- Unidade que controla os componentes da CPU
 
 library ieee;
@@ -10,31 +11,6 @@ entity ControlUnit is
 		muxALUI_A                   : out STD_LOGIC;                     -- mux que seleciona entre instrução e ALU para reg. A
 		muxAM_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
 		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
-		loadA, loadD, loadM, loadPC : out STD_LOGIC                     -- sinais de load do reg. A, reg. D, Mem. RAM e Program Counter
-		
-		
+		loadA, loadD, loadM, loadPC : out STD_LOGIC                      -- sinais de load do reg. A, reg. D, Mem. RAM e Program Counter
     );
 end entity;
-
-architecture CU of ControlUnit is
-
-begin	
-
-muxALUI_A <= not instruction(0);
-loadA <= not instruction(0) or instruction(10);
-muxAM_ALU <= instruction(3) and instruction(0);
-
-zx <= instruction(4) and instruction(0);
-nx <= instruction(5) and instruction(0);
-zy <= instruction(6) and instruction(0);
-ny <= instruction(7) and instruction(0);
-f <= instruction(8) and instruction(0);
-no <= instruction(9) and instruction(0);
-
-loadD <= instruction(11) and instruction(0);
-loadM <= instruction(12) and instruction(0);
-
-loadPC <= ((instruction(15) and not ng and not zr) or (instruction(14) and zr) or (instruction(13) and ng and not zr)) and instruction(0);
-	
-end architecture;
-	
