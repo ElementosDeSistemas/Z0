c49163141f9af5e1163ad184d4239cc035b704b9
Jean Low <j.luca@hotmail.com>
2017-04-13 16:08:39 -0300

    first test

--- Computador/Modulos/CPU/ControlUnit.vhd
+++ Computador/Modulos/CPU/ControlUnit.vhd
@@ -12,5 +12,31 @@ entity ControlUnit is
 		muxAM_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
 		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
 		loadA, loadD, loadM, loadPC : out STD_LOGIC                      -- sinais de load do reg. A, reg. D, Mem. RAM e Program Counter
+		
+		
     );
 end entity;
+
+architecture CU of ControlUnit is
+
+begin	
+
+muxALUI_A <= !instruction(0);
+loadA <= !instruction(0);
+muxAM_ALU <= instruction(3) and instruction(0);
+
+zx <= instruction(4) and instruction(0);
+nx <= instruction(5) and instruction(0);
+zy <= instruction(6) and instruction(0);
+ny <= instruction(7) and instruction(0);
+f <= instruction(8) and instruction(0);
+no <= instruction(9) and instruction(0);
+
+loadA <= instruction(10) or !instruction(0);
+loadD <= instruction(11) and instruction(0);
+loadM <= instruction(12) and instruction(0);
+
+loadPC <= (instruction(15) and !ng and !zr) or (instruction(14) and zr) or (instruction(13) and ng and !zr);
+	
+end architecture;
+	
