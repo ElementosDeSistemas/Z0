c1f60346e4b2a6ed7d6a3f31fef9d7209d96b8ef
Leostayner <leostayner@gmail.com>
2017-04-13 15:39:14 -0300

    leo e sabrina V.8

--- Computador/Modulos/CPU/PC.vhd
+++ Computador/Modulos/CPU/PC.vhd
@@ -1,5 +1,5 @@
 -- Contador de 16bits
--- if (reset[t] == 1) out[t+1] = 0
+-- if (reset[t] == 1) out[t+1] = 0.
 -- else if (load[t] == 1)  out[t+1] = in[t]
 -- else if (inc[t] == 1) out[t+1] = out[t] + 1
 -- else out[t+1] = out[t].
