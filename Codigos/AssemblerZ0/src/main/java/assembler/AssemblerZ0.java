5918046869ae418cacb77692b11caa57f6b72e0b
Leostayner <leostayner@gmail.com>
2017-05-08 11:08:49 -0300

    .

--- Codigos/AssemblerZ0/src/main/java/assembler/AssemblerZ0.java
+++ Codigos/AssemblerZ0/src/main/java/assembler/AssemblerZ0.java
@@ -2,9 +2,8 @@
  * Curso: Elementos de Sistemas
  * Arquivo: AssemblerZ0.java
  */
-
 package assembler;
-
+import java.io.BufferedReader;
 /**
  * Classe principal que orquestra execução do Assembler.
  * Opções:
@@ -14,32 +13,34 @@ package assembler;
 class AssemblerZ0 {
 	public Paser parser;
 	public Code  code;
-	public SymbolTable symbol;
+	public symbol_table;
 	public int linha;
-
-
+	
     public static void main(String[] args) {
     	parser = new Paser();
-    	linha = 0 
+    	linha = 0 ;
     	code = new Code();
-    	symbol = new SymbolTable();
+    	FileReader arq = new FileReader(nome);
+    	symbol_table = parser.getSymbolTable(arq);
+    	
     	while(parser.advance()){
-    		if (parser.CommandType == Parser.CommandType.L_Command){
-    			symbol.addEntry(parser.simbol(),linha);
+    		if (parser.CommandType() == Parser.CommandType.L_Command){
+    			symbol.addEntry(parser.Simbol(),linha);
     		}
 
-    		else if (parser.CommandType == Parser.CommandType.C_Command){
-    			linha++
-    			System.out.println("111" + code.comp(array) + code.dest(array))
+    		else if (parser.CommandType() == Parser.CommandType.C_Command){
+    			linha++;
+    			arq.write("111" + code.Comp(parser.Instruction(parser.Command)) + code.Dest(parser.Instruction(parser.Command)));
 
     	}
-    
-    		else if (parser.CommandType == Parser.CommandType.A_Command){
+    		else if (parser.CommandType() == Parser.CommandType.A_Command){
     			linha++
     			if (paser.isNum()){
-    				code.tobinary()
+    				code.tobinary();
     			}
     			else{
+    				//converter usando tabela	
+    				code.tobinary();
 
     			}
     			
